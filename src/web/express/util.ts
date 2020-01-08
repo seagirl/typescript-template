db015d743c28d76c5cec9dbff43047d25a12cd0c
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2020-01-08 16:17:42 +0900

    Builder の型指定は不要になった

--- src/web/express/util.ts
+++ src/web/express/util.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { Builder } from '../../core'
 
-export const handle = async (builder: Builder<object>, req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const handle = async (builder: Builder, req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const result = await builder.controller.handle(req)
     res.json(builder.presenter.present(result))
