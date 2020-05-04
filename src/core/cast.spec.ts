8a2175ed793169f12200f5bf8d2ce1164016ec90
seagirl <n0.sigur@gmail.com>
2020-05-04 12:42:10 +0900

    impl toBoolean

--- src/core/cast.spec.ts
+++ src/core/cast.spec.ts
@@ -1,15 +1,45 @@
-import { toEnum, toNumber } from './cast'
+import { toBoolean, toEnum, toNumber, toOptionalBoolean, toOptionalNumber } from './cast'
+import { ServerError } from './error'
 
 enum Hoge {
   fugaFuga = 'fuga_fuga'
 }
 
 describe('cast', () => {
+  it('toBoolean', () => {
+    expect(() => { toBoolean(undefined) }).toThrowError(ServerError)
+    expect(toBoolean(1)).toEqual(true)
+    expect(toBoolean(0)).toEqual(false)
+    expect(toBoolean('')).toEqual(false)
+    expect(toBoolean('true')).toEqual(true)
+    expect(toBoolean('false')).toEqual(true)
+    expect(toBoolean(true)).toEqual(true)
+    expect(toBoolean(false)).toEqual(false)
+  })
+
+  it('toOptionalBoolean', () => {
+    expect(toOptionalBoolean(undefined)).toEqual(undefined)
+    expect(toOptionalBoolean(1)).toEqual(true)
+    expect(toOptionalBoolean(0)).toEqual(false)
+    expect(toOptionalBoolean('')).toEqual(false)
+    expect(toOptionalBoolean('true')).toEqual(true)
+    expect(toOptionalBoolean('false')).toEqual(true)
+    expect(toOptionalBoolean(true)).toEqual(true)
+    expect(toOptionalBoolean(false)).toEqual(false)
+  })
+
   it('toNumber', () => {
-    expect(toNumber(undefined)).toEqual(undefined)
-    expect(toNumber('fugaFuga')).toEqual(undefined)
+    expect(() => { toNumber(undefined) }).toThrowError(ServerError)
+    expect(() => { toNumber('fugaFuga') }).toThrowError(ServerError)
     expect(toNumber('1234')).toEqual(1234)
     expect(toNumber(1234)).toEqual(1234)
+  })
+
+  it('toOptionalNumber', () => {
+    expect(toOptionalNumber(undefined)).toEqual(undefined)
+    expect(toOptionalNumber('fugaFuga')).toEqual(undefined)
+    expect(toOptionalNumber('1234')).toEqual(1234)
+    expect(toOptionalNumber(1234)).toEqual(1234)
   })
 
   it('toEnum', () => {
--- src/core/cast.ts
+++ src/core/cast.ts
@@ -1,4 +1,38 @@
-export function toNumber (value: number | string | undefined): number | undefined {
+import { ServerError } from './error'
+
+export function toBoolean (value: boolean | number | string | undefined): boolean {
+  if (value == null) {
+    throw new ServerError('value must be specified.')
+  }
+
+  const bool = Boolean(value)
+  return bool
+}
+
+export function toOptionalBoolean (value: boolean | number | string | undefined): boolean | undefined {
+  if (value == null) {
+    return
+  }
+
+  const bool = Boolean(value)
+  return bool
+}
+
+export function toNumber (value: number | string | undefined): number {
+  if (value == null) {
+    throw new ServerError('value must be specified.')
+  }
+
+  const number = Number(value)
+  // return undefined if value is NaN
+  if (number !== number) {
+    throw new ServerError(`${value} is not a number`)
+  }
+
+  return number
+}
+
+export function toOptionalNumber (value: number | string | undefined): number | undefined {
   if (value == null) {
     return
   }
