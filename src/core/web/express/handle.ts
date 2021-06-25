02097af9f45eba6addb0b4e7083a6e97bf01ac39
seagirl <n0.sigur@gmail.com>
2021-06-25 20:04:16 +0900

    wip: modify

--- src/core/web/express/handle.ts
+++ src/core/web/express/handle.ts
@@ -1,15 +1,25 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { Handler } from './handler'
 
-export const handle = async (builder: Handler, req: Request, res: Response, next: NextFunction): Promise<void> => {
+interface PromiseRequestHandler {
+  (req: Request, res: Response, next: NextFunction): Promise<void>;
+}
+
+export const wrap = (fn: PromiseRequestHandler): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    fn(req, res, next).catch(next)
+  }
+}
+
+export const handle = async (handler: Handler, req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const result = await builder.controller.handle({
+    const result = await handler.controller.handle({
       query: req.query,
       params: req.params,
       body: req.body,
       url: `${req.protocol}://${req.hostname}${req.originalUrl}`
     })
-    res.json(builder.presenter.present(result))
+    res.json(handler.presenter.present(result))
   } catch (error) {
     next(error)
   }
--- src/web/express/api/member.api.ts
+++ src/web/express/api/member.api.ts
@@ -1,26 +1,26 @@
 import { NextFunction, Request, Response, Router } from 'express'
-import { handle } from '../../../../core/web/express/handle'
-import { DeleteMemberHandler } from './delete-member.handler'
-import { GetMemberHandler } from './get-member.handler'
-import { GetMembersHandler } from './get-members.handler'
-import { PostMembersHandler } from './post-members.handler'
+import { handle, wrap } from '../../../core/web/express/handle'
+import { DeleteMemberHandler } from './member/delete-member.handler'
+import { GetMemberHandler } from './member/get-member.handler'
+import { GetMembersHandler } from './member/get-members.handler'
+import { PostMembersHandler } from './member/post-members.handler'
 
 const router = Router()
 
-router.get('/members', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/members', wrap(async (req: Request, res: Response, next: NextFunction) => {
   await handle(new GetMembersHandler(), req, res, next)
-})
+}))
 
-router.get('/members/:code', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/members/:code', wrap(async (req: Request, res: Response, next: NextFunction) => {
   await handle(new GetMemberHandler(), req, res, next)
-})
+}))
 
-router.post('/members', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/members', wrap(async (req: Request, res: Response, next: NextFunction) => {
   await handle(new PostMembersHandler(), req, res, next)
-})
+}))
 
-router.delete('/members/:code', async (req: Request, res: Response, next: NextFunction) => {
+router.delete('/members/:code', wrap(async (req: Request, res: Response, next: NextFunction) => {
   await handle(new DeleteMemberHandler(), req, res, next)
-})
+}))
 
 export default router
